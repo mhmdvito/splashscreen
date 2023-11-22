8de57dfdffe8f3dfb2234f0c9c6cd8f7bcee80d7
mhmdvito <muhammad.vitoo176@gmail.com>
2023-11-22 23:35:36 +0700

    first

---
+++ app/src/main/java/com/example/splash/Intropage1.kt
@@ -0,0 +1,11 @@
+package com.example.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Intropage1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intropage1)
+    }
+}
---
+++ app/src/main/java/com/example/splash/Intropage3.kt
@@ -0,0 +1,11 @@
+package com.example.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Intropage3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intropage3)
+    }
+}
